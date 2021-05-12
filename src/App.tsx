fe5de348e8963703f57b11bac3d648f8c55af531
lark698 <dingzhao1995@gmail.com>
2021-05-12 22:41:02 +0800

    feat：网站初始化。

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from "./App.module.css";
+import {Header, Footer, SideMenu} from "./components";
+import {Row, Col} from "antd";
+import {Carousel} from "./components/carousel";
+
+function App() {
+    return (
+        <div className={styles.App}>
+            <Header />
+            {/* 页面内容 content */}
+            <div className={styles["page-content"]}>
+                <Row style={{ marginTop: 20 }}>
+                    <Col span={6}>
+                        <SideMenu />
+                    </Col>
+                    <Col span={18}>
+                        <Carousel/>
+                    </Col>
+                </Row>
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
